5b3035e72ed3e4501a121c61b190c36c311ea5f1
gmshake <gmshake@7480b342-bb24-6c17-0daa-b1491cfa11c5>
2011-04-26 17:10:16 +0000

    add missing yixun_config.h

---
+++ trunk/src/yixun_config.h
@@ -0,0 +1,35 @@
+/*
+ * yixun_config.h
+ *
+ * by Summer Town
+ * 2011.04.26
+ */
+
+#ifndef YIXUN_CONFIG_H
+#define YIXUN_CONFIG_H
+
+#ifndef CONF_LEN
+#define CONF_LEN 32
+#endif
+
+extern char username[CONF_LEN];
+extern char password[CONF_LEN];
+extern char hwaddr[CONF_LEN];
+extern char regip[CONF_LEN];
+
+extern char authserver[CONF_LEN];
+extern char msgserver[CONF_LEN];
+
+extern unsigned int serverport;
+extern unsigned int listenport;
+
+extern time_t conn_timeout;
+extern time_t snd_timeout;
+extern time_t rcv_timeout;
+extern time_t heart_beat_timeout;
+
+extern int pre_config_done;
+
+
+#endif	/* YIXUN_CONFIG_H */
+
